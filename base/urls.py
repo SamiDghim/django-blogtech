50df8a36a732ee50f56b09ada9a7e6ad58a2923d
Sami Dghim <samidghim@gmail.com>
2024-05-19 01:24:27 +0200

    impement login/logout

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 
 urlpatterns = [
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+
+    
     path('', views.home, name="home"), # (direct path, views, name)
     path('room/<str:pk>/' , views.room, name='room'), # we add name to access to url by name
 
