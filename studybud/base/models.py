31ee7b6bb633abb554ce0d1c8b53d9dacb808688
Sami Dghim <samidghim@gmail.com>
2024-05-18 15:33:28 +0200

    create superuser

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# Room table
+
+class Room(models.Model):
+  # host =
+  # topic =
+  name = models.CharField(max_length=200)
+  description = models.TextField(null=True, blank=True) # null : can save room with empty description / blank : can save form with description empty 
+  # participants
+  updated = models.DateTimeField(auto_now=True) # take snapchot every time we save the room
+  created = models.DateTimeField(auto_now_add=True) # take snapchot when we first save/create room
+
+  # string representation of the room
+  def __str__(self) -> str:
+    return self.name
