8e5885e9464d3aa3de5edc121491172ffc03155f
Sami Dghim <samidghim@gmail.com>
2024-05-18 16:23:46 +0200

    create room form and save in database

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"), # (direct path, views, name)
     path('room/<str:pk>/' , views.room, name='room'), # we add name to access to url by name
+
+    path('create-room/', views.createRoom, name="create-room")
 ]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
 from .models import Room
+from .forms import RoomForm
 # Create your views here.
 
 # rooms = [
@@ -18,3 +19,16 @@ def room(request,pk):
 
   context = {"room": room}
   return  render(request, 'base/room.html', context)
+
+
+
+def createRoom(request):
+  form = RoomForm()
+  if request.method == 'POST':
+    form = RoomForm(request.POST) # set
+    if form.is_valid():
+      form.save()
+      return redirect('home')
+
+  context = {'form': form}
+  return render(request, 'base/room_form.html', context)
