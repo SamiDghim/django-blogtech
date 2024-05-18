6210fb60b42261535e52ed766de84722dab68199
Sami Dghim <samidghim@gmail.com>
2024-05-18 16:05:25 +0200

    add more information to home page

--- studybud/base/admin.py
+++ studybud/base/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Room
+from .models import Topic
+from .models import Message
 
 # this add the model Room to admin Panel
 admin.site.register(Room)
+admin.site.register(Topic)
+admin.site.register(Message)
 
--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,18 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-# Room table
-
 class Room(models.Model):
-  # host =
-  # topic =
+  host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True) 
+  topic =  models.ForeignKey('Topic', on_delete=models.SET_NULL, null=True) 
   name = models.CharField(max_length=200)
-  description = models.TextField(null=True, blank=True) # null : can save room with empty description / blank : can save form with description empty 
+  description = models.TextField(null=True, blank=True) # null : can save room with empty description (default value is false) / blank : can save form with description empty (default value is false) 
   # participants
   updated = models.DateTimeField(auto_now=True) # take snapchot every time we save the room
   created = models.DateTimeField(auto_now_add=True) # take snapchot when we first save/create room
 
   # string representation of the room
-  def __str__(self) -> str:
+  def __str__(self):
     return self.name
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+      return self.name
+
+class Message(models.Model):
+  # https://docs.djangoproject.com/en/5.0/ref/contrib/auth/
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  room = models.ForeignKey(Room, on_delete=models.CASCADE) # one to many relationship / SET_NULL: when parent deleted set ForeignKey to null /
+  body = models.TextField()
+  updated = models.DateTimeField(auto_now=True) # take snapchot every time we save the room
+  created = models.DateTimeField(auto_now_add=True) # take snapchot when we first save/create room
+
+  def __str__(self):
+    return self.body[0:50] # limit to 50 character
