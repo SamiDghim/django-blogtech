a619c7067fd1a71bc3d32912f21c098705224b6c
Sami Dghim <samidghim@gmail.com>
2024-05-19 15:08:25 +0200

    add registration

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
-
+    path('register/', views.registerPage, name="register"),
     
     path('', views.home, name="home"), # (direct path, views, name)
     path('room/<str:pk>/' , views.room, name='room'), # we add name to access to url by name
--- base/views.py
+++ base/views.py
@@ -6,6 +6,7 @@ from  django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 from django.contrib.auth import authenticate, login, logout
@@ -17,11 +18,12 @@ from django.contrib.auth import authenticate, login, logout
 # ]
 
 def loginPage(request):
+  page = 'login'
   if request.user.is_authenticated:
     return redirect('home')
 
   if request.method == 'POST':
-    username = request.POST.get('username')
+    username = request.POST.get('username').lower()
     password = request.POST.get('password')
  
     try:
@@ -36,12 +38,28 @@ def loginPage(request):
       return redirect('home')
     else:
       messages.error(request, "Username or password does not exist")
-
-  return  render(request, 'base/login_register.html', )
+  context = {'page': page}
+  return  render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
   logout(request)
   return redirect('home')
+
+def registerPage(request):
+  form = UserCreationForm()
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save(commit=False) # save form and freez it in time/ not save in database yet
+      user.username = user.username.lower()
+      user.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      messages.error(request, 'An error occured during registration')
+
+  return  render(request, 'base/login_register.html', { 'form': form})
+
 
 
 def home(request):
