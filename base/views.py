22dcc9320fd6099d0a5b8a87c7a6ff35d9a1a9fe
Sami Dghim <samidghim@gmail.com>
2024-05-19 15:25:53 +0200

    add message create/show

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth import authenticate, login, logout
 
@@ -60,8 +60,6 @@ def registerPage(request):
 
   return  render(request, 'base/login_register.html', { 'form': form})
 
-
-
 def home(request):
   q = request.GET.get('q') if request.GET.get('q') != None else ''
 
@@ -78,8 +76,18 @@ def home(request):
 
 def room(request,pk):
   room = Room.objects.get(id=pk)
+  room_messages = room.message_set.all().order_by('-created')
+  if request.method == 'POST':
+    message = Message.objects.create(
+      user= request.user,
+      room= room,
+      body= request.POST.get('body')
+    )
+    return redirect('room',pk=room.id)
 
-  context = {"room": room}
+
+
+  context = {"room": room, 'room_messages': room_messages}
   return  render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
